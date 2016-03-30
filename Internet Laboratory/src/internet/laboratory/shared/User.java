1903f15f68f3ca9e1d125e1e6dcd8e4a419c0e4c
josalgrod <wifialgarin@gmail.com>
2016-03-30 20:15:15 +0200

    User checks

--- Internet Laboratory/src/internet/laboratory/shared/User.java
+++ Internet Laboratory/src/internet/laboratory/shared/User.java
@@ -16,7 +16,6 @@ import com.google.appengine.api.datastore.Key;
 
 public class User implements Serializable {
 
-	
 	/**
 	 * 
 	 */
@@ -50,12 +49,17 @@ public class User implements Serializable {
 	public User(String username, String password, String name,
 			GregorianCalendar birthdate, String phone, String email) {
 		super();
-		Username = username;
-		Password = password;
-		Name = name;
-		Birthdate = birthdate;
-		Phone = phone;
-		Email = email;
+
+		if (checkUserName(username) && checkPassword(password)
+				&& checkName(name) && checkBirthdate(birthdate)
+				&& checkPhone(phone) && checkEmail(email)) {
+			Username = username;
+			Password = password;
+			Name = name;
+			Birthdate = birthdate;
+			Phone = phone;
+			Email = email;
+		}
 	}
 
 	public String getUsername() {
@@ -81,7 +85,7 @@ public class User implements Serializable {
 	public void setName(String name) {
 		Name = name;
 	}
-	
+
 	public GregorianCalendar getBirthdate() {
 		return Birthdate;
 	}
@@ -110,6 +114,53 @@ public class User implements Serializable {
 		return Id_User;
 	}
 
-	@OneToMany(mappedBy = "User_Event", cascade = CascadeType.REFRESH, fetch=FetchType.LAZY) 
+	private boolean checkEmail(String email) {
+		return !email.isEmpty() && email.contains("@");
+	}
+
+	private boolean checkPhone(String phone) {
+		return phone.length() == 9 && Character.isDigit(0)
+				&& Character.isDigit(1) && Character.isDigit(2)
+				&& Character.isDigit(3) && Character.isDigit(4)
+				&& Character.isDigit(5) && Character.isDigit(6)
+				&& Character.isDigit(7) && Character.isDigit(8);
+	}
+
+	private boolean checkBirthdate(GregorianCalendar birthdate) {
+		GregorianCalendar current = new GregorianCalendar();
+		return birthdate.before(current);
+	}
+
+	private boolean checkName(String name) {
+		boolean res = false;
+		for (int i = 0; i < name.length(); i++) {
+			char espace = ' ';
+			if (Character.isAlphabetic(name.charAt(i))
+					|| name.charAt(i) == espace) {
+				res = true;
+			}
+		}
+		return res;
+	}
+
+	private boolean checkPassword(String password) {
+		boolean res = false;
+		for (int i = 0; i < password.length(); i++) {
+			char espace = ' ';
+			if (Character.isAlphabetic(password.charAt(i))
+					|| Character.isDigit(password.charAt(i))
+					&& password.charAt(i) != espace && password.length() > 5
+					&& password.length() < 16) {
+				res = true;
+			}
+		}
+		return res;
+	}
+
+	private boolean checkUserName(String username) {
+		return !username.isEmpty();
+	}
+
+	@OneToMany(mappedBy = "User_Event", cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
 	private Set<Event_User> userEvent;
 }
