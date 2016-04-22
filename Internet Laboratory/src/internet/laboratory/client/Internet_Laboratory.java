5008ed02b8af0c8cacdbcd9d42b79016a8974a01
josalgrod <wifialgarin@gmail.com>
2016-04-22 11:17:02 +0200

    Base HTML

--- Internet Laboratory/src/internet/laboratory/client/Internet_Laboratory.java
+++ Internet Laboratory/src/internet/laboratory/client/Internet_Laboratory.java
@@ -1,22 +1,26 @@
 package internet.laboratory.client;
 
-import internet.laboratory.shared.FieldVerifier;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.SplitLayoutPanel;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.TextBox;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -40,24 +44,77 @@ public class Internet_Laboratory implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
 		final Label errorLabel = new Label();
-
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
 
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
 		RootPanel rootPanel = RootPanel.get("nameFieldContainer");
-		rootPanel.add(nameField, 121, 50);
-		RootPanel.get("sendButtonContainer").add(sendButton, 121, 150);
 		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
+		
+		FlowPanel rightPanel = new FlowPanel();
+		rootPanel.add(rightPanel, 0, 100);
+		rightPanel.setSize("154px", "400px");
+		
+		Button btnCreate = new Button("Create Event");
+		rightPanel.add(btnCreate);
+		btnCreate.setWidth("153px");
+		
+		Button btnUpdate = new Button("Update Event");
+		rightPanel.add(btnUpdate);
+		btnUpdate.setWidth("153px");
+		
+		Button btnDelete = new Button("Delete Event");
+		rightPanel.add(btnDelete);
+		btnDelete.setWidth("153px");
+		
+		AbsolutePanel topPanel = new AbsolutePanel();
+		rootPanel.add(topPanel, 0, 0);
+		topPanel.setSize("685px", "100px");
+		
+		Label lbTitle = new Label("Party&Go");
+		lbTitle.setStyleName("h1");
+		topPanel.add(lbTitle, 264, 0);
+		
+		Hyperlink logout = new Hyperlink("Logout", false, "newHistoryToken");
+		topPanel.add(logout, 636, 10);
+		
+		Label lbNameUser = new Label("Name");
+		topPanel.add(lbNameUser, 577, 61);
+		
+		Image image = new Image((String) null);
+		topPanel.add(image, 511, 52);
+		image.setSize("47px", "38px");
+		
+		Button btnSettings = new Button("");
+		topPanel.add(btnSettings, 636, 61);
+		btnSettings.setSize("16px", "18px");
+		
+		TextBox txtbxSearch = new TextBox();
+		txtbxSearch.setText("search");
+		topPanel.add(txtbxSearch, 266, 54);
+		txtbxSearch.setSize("142px", "18px");
+		
+		AbsolutePanel leftPanel = new AbsolutePanel();
+		rootPanel.add(leftPanel, 513, 100);
+		leftPanel.setSize("172px", "400px");
+		
+		HTML googleMaps = new HTML("Google Maps", true);
+		leftPanel.add(googleMaps, 10, 31);
+		googleMaps.setSize("152px", "121px");
+		
+		AbsolutePanel centerPanel = new AbsolutePanel();
+		rootPanel.add(centerPanel, 153, 100);
+		centerPanel.setSize("361px", "400px");
+		
+		ListBox listEvents = new ListBox();
+		centerPanel.add(listEvents, 0, 30);
+		listEvents.setSize("361px", "370px");
+		listEvents.setVisibleItemCount(5);
+		
+		Label lblNewLabel = new Label("Your events");
+		lblNewLabel.setStyleName("h2");
+		centerPanel.add(lblNewLabel, 126, 0);
+		lblNewLabel.setSize("107px", "30px");
 
 		// Create the popup dialog box
 		final DialogBox dialogBox = new DialogBox();
@@ -82,73 +139,7 @@ public class Internet_Laboratory implements EntryPoint {
 		closeButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
 			}
 		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
 	}
 }
